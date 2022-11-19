cc5ad4c98b45da76a636a12cce449232a26578f3
Lisandra Cruz <lisansouza@gmail.com>
2022-11-19 09:39:02 -0300

    Implementação do contador sem o Palpiteiro

--- contador-palpiteiro/src/components/Contador.jsx
+++ contador-palpiteiro/src/components/Contador.jsx
@@ -1,4 +1,25 @@
+import react, { useState } from "react";
+import Palpiteiro from "./Palpiteiro";
+
 const ContadorPalpiteiro = () => {
-  return <></>;
+  const [numero, setNumero] = useState(0);
+
+  const somarUm = () => {
+    setNumero((valor) => valor + 1);
+  };
+
+  const subtrairUm = () => {
+    setNumero((valor) => valor - 1);
+  };
+
+  return (
+    <>
+      <button onClick={somarUm}>+</button>
+      <p>{numero}</p>
+      <button onClick={subtrairUm}>-</button>
+      <br />
+      <Palpiteiro  />
+    </>
+  );
 };
 export default ContadorPalpiteiro;
